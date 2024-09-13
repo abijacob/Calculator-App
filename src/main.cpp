76ca4f6005738e2eb4d76628a8e871fa4ae1ad8c
Abi Jacob <abijacob@Abis-MacBook-Pro-2.local>
2024-09-13 01:57:27 -0700

    Basic Operations Added

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 
 int main() {
-    cout<< "Initializing Calculator App!" << endl;
+    double num1, num2;
+    char operation;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/' :
+            if(num2 != 0) {
+                cout << "Result: " << num1 / num2 << endl;
+            }
+            else {
+                cout << "Error! Cannot divide by 0." << endl;
+                break;
+            }
+        default:
+            cout << "Invalid operation!" << endl;
+    }
     return 0;
 }
