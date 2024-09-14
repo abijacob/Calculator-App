4eb78a771bdd90be0017dc3da22fefb7b8759a1b
Abi Jacob <abijacob@Abis-MacBook-Pro-2.local>
2024-09-13 21:30:28 -0700

    Added a basic menu and fixed error in division operation

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,57 @@
 #include <iostream>
 using namespace std;
 
+void displayMenu() {
+  cout << "Simple Calculator\n";
+  cout << "1. Perform a Calculation\n";
+  cout << "2. Exit\n";
+  cout << "Choose an option: ";
+}
+
 int main() {
-    double num1, num2;
-    char operation;
+  double num1, num2;
+  char operation;
+  int option;
 
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter operation (+, -, *, /): ";
-    cin >> operation;
-    cout << "Enter second number: ";
-    cin >> num2;
+  do {
+    displayMenu();
+    cin >> option;
 
-    switch (operation) {
-        case '+':
-            cout << "Result: " << num1 + num2 << endl;
-            break;
-        case '-':
-            cout << "Result: " << num1 - num2 << endl;
-            break;
-        case '*':
-            cout << "Result: " << num1 * num2 << endl;
-            break;
-        case '/' :
-            if(num2 != 0) {
-                cout << "Result: " << num1 / num2 << endl;
-            }
-            else {
-                cout << "Error! Cannot divide by 0." << endl;
-                break;
-            }
-        default:
-            cout << "Invalid operation!" << endl;
+    if (option == 1) {
+      cout << "Enter the first number: ";
+      cin >> num1;
+      cout << "Enter operation (+, -, *, /): ";
+      cin >> operation;
+      cout << "Enter second number: ";
+      cin >> num2;
+
+      switch (operation) {
+      case '+':
+        cout << "Result: " << num1 + num2 << endl;
+        break;
+      case '-':
+        cout << "Result: " << num1 - num2 << endl;
+        break;
+      case '*':
+        cout << "Result: " << num1 * num2 << endl;
+        break;
+      case '/':
+        if (num2 != 0) {
+          cout << "Result: " << num1 / num2 << endl;
+          break;
+        } else {
+          cout << "Error! Cannot divide by 0." << endl;
+          break;
+        }
+      default:
+        cout << "Invalid operation!" << endl;
+      }
+    } else if (option != 2) {
+      cout << "Invalid option! Please choose again.\n";
     }
-    return 0;
+  } while (option != 2);
+
+  cout << "Exiting the calculator.\n";
+
+  return 0;
 }
